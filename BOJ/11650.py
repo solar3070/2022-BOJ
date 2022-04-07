4b26fdb691f0bb13c8c0a924333750f345281c73
HyeJun <solar3070@naver.com>
2022-04-08 00:00:48 +0900

    220408

--- BOJ/11650.py
+++ BOJ/11650.py
@@ -1,9 +1,10 @@
+from audioop import reverse
 import sys
 n = int(input())
 lst = []
 for _ in range(n):
   lst.append(list(map(int, sys.stdin.readline().split())))
 
-lst.sort()
+lst.sort(key = lambda x: (x[1], x[0]))
 for a, b in lst:
   print(a, b)
