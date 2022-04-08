71e697dae8495ef29384b52e85cfe12691757145
HyeJun <solar3070@naver.com>
2022-04-09 04:01:49 +0900

    220409

--- BOJ/1181.py
+++ BOJ/1181.py
@@ -4,18 +4,6 @@ word = [sys.stdin.readline().strip() for _ in range(n)]
 
 word = list(set(word))
 word.sort(key=len)
+word.sort()
 
-length = 1
-tmp, result = [], []
-for w in word:
-  if len(w) != length:
-    length = len(w)
-    tmp.sort()
-    result += tmp
-    tmp = [w]
-  else:
-    tmp.append(w) 
-tmp.sort()
-result += tmp
-
-for word in result: print(word)
+for w in word: print(w)
