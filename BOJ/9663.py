33480b0f5acdfc7763d7e65c2d095ccd4d06d3d3
HyeJun <solar3070@naver.com>
2022-04-18 20:02:23 +0900

    220418

---
+++ BOJ/9663.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+board = [0] * n
+cnt = 0
+
+def check_board(idx):
+  for i in range(idx):
+    if board[idx] == board[i] or board[idx] + idx == board[i] + i or board[idx] - idx == board[i] - i:
+      return False
+  return True
+
+def set_queen(idx):
+  global cnt
+
+  if idx == n:
+    cnt += 1
+    return
+
+  for i in range(n):
+    board[idx] = i
+    if check_board(idx):
+      set_queen(idx + 1)
+
+set_queen(0)
+print(cnt)
